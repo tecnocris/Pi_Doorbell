0a56cd0d17ea08bd23078413aa9e3aa73431a681
Christopher May <15093217+tecnocris@users.noreply.github.com>
2020-01-24 13:30:53 -0600

    button_v.0.02.py
    Updated code to utilize gpiozero library instead of gpio library. Also now works with a passive infrared sensor as a trigger for camera as well as fall back redundancy to utilize the doorbell or other button in case the sensor fails to detect.

--- button.py
+++ button.py
@@ -1,22 +1,28 @@
 import camera_module
-import RPi.GPIO as GPIO
+from gpiozero import MotionSensor, Button, LED
+from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-ledPin = 12
-buttonPin = 16
-GPIO.setup(ledPin, GPIO.OUT)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Create object for button
+# Button is on GPIO-2 (Pin 3)
+button = Button(2)
+# Create object for led
+# LED is on GPIO-3 (Pin 5)
+led = LED(3)
+# Create object for PIR Sensor
+#PIR Sensor is on GPIO-4 (Pin 7)
+pir = MotionSensor(4)
 
 while True:
-    buttonState = GPIO.input(buttonPin)
-    if buttonState == False:
-        GPIO.output(ledPin, GPIO.HIGH)
+    if button.is_pressed:
+        print("button pressed")
+        led.on()
+        sleep(1)
+        led.off()
         camera_module.doorbell_clip()
-        #print("it's alive")
-    else:
-        GPIO.output(ledPin, GPIO.LOW)
-        #print("she's dead, jim")
-        
-
-
+    elif pir.motion_detected == True:
+        print("Heat Detected")
+        led.on()
+        sleep(1)
+        led.off()
+        camera_module.doorbell_clip()
